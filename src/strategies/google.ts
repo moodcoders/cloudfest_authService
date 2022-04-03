1d54a077e5ea22d9623327fbe3943b66c0eedf07
osama bin junaid <osama.junaid@c3m.io>
2022-04-03 18:30:58 +0530

    chnaged google oAuth routes

--- src/strategies/google.ts
+++ src/strategies/google.ts
@@ -3,19 +3,27 @@ import { Profile, Strategy, VerifyCallback } from 'passport-google-oauth20';
 import UserModel from '../models/users';
 
 const verify = async (accessToken:string, refreshToken: string, profile: Profile, cb: VerifyCallback): Promise<void> => {
-    console.log(accessToken, refreshToken, profile);
+
     try {
         const user = await UserModel.findOne({
             'providers.name': profile.provider.toUpperCase(),
             'providers.uid': profile.id
         });
+
         if (user === null) {
             const newUser = new UserModel({
                 name: profile.displayName,
                 email: profile.emails ? profile.emails[0].value : '',
+                providers: [{
+                    name: profile.provider.toUpperCase(),
+                    uid: profile.id,
+                }]
             })
             const savedUser = await newUser.save();
             cb(null, savedUser);
+        } else {
+            //the user already exists in db
+            cb(null, user);
         }
     } catch (error: any) {
         console.log(error)
@@ -27,7 +35,7 @@ const googleStrategy: Strategy = new Strategy(
     {
         clientID: '363336576338-cdn32827l9fthvubbhpenn98eb649lsd.apps.googleusercontent.com',
         clientSecret: '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',
-        callbackURL: 'http://localhost:4000/auth/google/callback'
+        callbackURL: 'http://192.168.1.12.xip.io:4000/auth/google/callback'
     },
     verify
 );
