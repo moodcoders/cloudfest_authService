9504866d3abf86de7dd4edab3097a955e3a70e0e
suchetamahata <nosuchetamahatano@gmail.com>
2022-04-08 09:42:24 +0530

    joi validation

--- src/routes/otp-routes.ts
+++ src/routes/otp-routes.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response, NextFunction } from 'express';
+
 import passport from 'passport';
 import { ErrorCode, HandyManError } from '../utils/error';
 import logger from '../utils/logger';
 import { generateOTPController } from './../controllers/otp-controller';
+import { validateOtpSchema } from '../joi-schemas/requestSchema';
 
 const OTPRouter: Router = Router();
 
@@ -10,19 +12,23 @@ OTPRouter.post('/generate-otp', generateOTPController);
 
 OTPRouter.post('/userlogin', passport.authenticate('userOtp', { session: false, failWithError: true }),
     async (req: Request, res: Response, next: NextFunction) => {
-        return res.json({ user: req.user })
+        const request = await validateOtpSchema.validateAsync(req.body)
+        return res.json({ user: request.user })
     },
     async (error: any, req: Request, res: Response, next: NextFunction) => {
+        const request = await validateOtpSchema.validateAsync(req.body)
         logger.error(error.message);
         if (error instanceof HandyManError && error.code === ErrorCode.BAD_DATA) {
             res.status(400).json({ message: error.message });
             return;
-        } else {
-            res.status(500).json({
-                message: 'Server Error'     //TODO: Write a method to process all errors
-            });
-        }
-        res.send(req.user);
+        } else if (error.isJoi === true) {
+            res.status(400).send(error.message)
+            return;
+        };
+        res.status(500).json({
+            message: 'Server Error'     //TODO: Write a method to process all errors
+        })
+        res.send(request.user);
     }
 );
 
