dec99922988e379fb77b622344eb4a2c4b547c6f
suchetamahata <nosuchetamahatano@gmail.com>
2022-04-11 11:01:18 +0530

    changed response message type

--- src/controllers/handymanControllers.ts
+++ src/controllers/handymanControllers.ts
@@ -4,7 +4,7 @@ import handyman from '../models/handyman';
 
 export const verifyHandyman = async (req: Request, res: Response) => {
     try {
-        const verified = await handyman.findByIdAndUpdate(req.query.id, req.body); //TODO: change the model from customer to handyman
+        const verified = await handyman.findByIdAndUpdate(req.query.id, req.body); 
         res.send(`handyman verified status changed to ${req.body.isVerified}`);
     } catch (error: any) {
         res.sendStatus(400);
